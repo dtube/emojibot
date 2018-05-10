0dfdb48b0373dc83b4b2bff30a11a9839b21dcb9
Adrien M <adrien@d.tube>
2018-05-11 00:19:03 +0200

    change upvote rules

--- upvote.js
+++ upvote.js
@@ -1,4 +1,4 @@
-var baseVoteWeight = 2000
+var baseVoteWeight = 2500
 var malusPerVote = 1 / Math.pow(2, 1/7) // 7 votes a week divides vote weight by 2
 var steem = require('steem')
 steem.api.setOptions({ url: 'https://api.steemit.com' })
@@ -13,16 +13,19 @@ connection.connect();
 
 var query = 'SELECT COUNT(*) as c FROM vote WHERE author = \''+process.argv[2]+'\' AND stamp >= DATE(NOW()) - INTERVAL 7 DAY;'
 connection.query(query, function (err, res, fields) {
-	var malusVotes = Math.pow(malusPerVote, res[0].c)
+	var c = res[0].c
+	var malusVotes = 1
+	if (c > 3) 
+		malusVotes = Math.pow(malusPerVote, c-3)
 	steem.api.getAccounts(['dtube', process.argv[2]], function(err, res) {
 		if (res[0].voting_power > 8000)
 			steem.api.getContent(process.argv[2], process.argv[3], function(err, res) {
-				// todo logic calculate voteWeight
 				var curators = process.argv[4].split(',')
 				var voteWeight = baseVoteWeight*curators.length
 				voteWeight *= malusVotes
 				voteWeight = 100*Math.round(voteWeight/100)
 				if (voteWeight > 10000) voteWeight = 10000
+				console.log(voteWeight)
 				steem.broadcast.vote(
 					'xxx',
 					'curator',
