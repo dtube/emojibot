94adeb2b9889347e918f499110e09779fa035aa7
Adrien M <adrien@d.tube>
2018-05-15 04:15:08 +0200

    fix downvote

--- downvote.js
+++ downvote.js
@@ -6,6 +6,7 @@ steem.api.getAccounts(['dtube', process.argv[2]], function(err, res) {
 	if (res[0].voting_power > 80)
 		steem.api.getContent(process.argv[2], process.argv[3], function(err, res) {
 			var voteweight = Math.floor(10000 * parseFloat(res.pending_payout_value.split(' ')[0]) / full_vote_dollar)
+			if (voteweight < 100) voteweight = 100
 			steem.broadcast.vote(
 				'xxx',
 				'curator',
